e6cd24ea6f5fa3018c7038f347de3ea66f0a6b19
Sarzy <adigwegodswillchinonso@gmail.com>
2021-07-18 16:11:03 +0100

    fixed splitting logic

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const matches = fs
   .readFileSync('football.csv', {
     encoding: 'utf-8',
   })
-  .split('/n')
+  .split('\n')
   .map((row: string): string[] => {
     return row.split(',');
   });
