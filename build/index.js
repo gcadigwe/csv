c781cd6e32ce1a26e9ab2f5fee26befbea2ac594
Sarzy <adigwegodswillchinonso@gmail.com>
2021-07-20 18:29:52 +0100

    abstract class and generics

--- build/index.js
+++ build/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var CsVFileReader_1 = require("./CsVFileReader");
+var MatchReader_1 = require("./MatchReader");
 var MatchResult_1 = require("./MatchResult");
-var reader = new CsVFileReader_1.CsvFileReader('football.csv');
+var reader = new MatchReader_1.MatchReader('football.csv');
 reader.read();
 var manUnitedWins = 0;
 for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
