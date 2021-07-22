6c1d3740e84711372bee3f92a6bb677e43a4cca2
Sarzy <adigwegodswillchinonso@gmail.com>
2021-07-22 15:22:34 +0100

    implement new method in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const csvFileReader = new CsvFileReader('football.csv');
+
+const matchreader = new MatchReader(csvFileReader);
+matchreader.load();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchreader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
