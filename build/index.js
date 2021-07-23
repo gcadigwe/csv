5a104971a91723eca2fc6d34914ace1e35a15174
Sarzy <adigwegodswillchinonso@gmail.com>
2021-07-23 16:32:25 +0100

    html report class

--- build/index.js
+++ build/index.js
@@ -2,19 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
 var CsvFileReader_1 = require("./CsvFileReader");
-var ConsoleReport_1 = require("./ReportTarget/ConsoleReport");
 var WinsAnalyzer_1 = require("./Analyzers/WinsAnalyzer");
 var Summary_1 = require("./Summary");
+var HtmlReport_1 = require("./ReportTarget/HtmlReport");
 var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 var matchreader = new MatchReader_1.MatchReader(csvFileReader);
 matchreader.load();
-// let manUnitedWins = 0;
-// for (let match of matchreader.matches) {
-//   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-//     manUnitedWins++;
-//   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-//     manUnitedWins++;
-//   }
-// }
-var summary = new Summary_1.Summary(new WinsAnalyzer_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+var summary = new Summary_1.Summary(new WinsAnalyzer_1.WinsAnalysis('Man United'), new HtmlReport_1.HtmlReport());
 summary.buildAndPringReport(matchreader.matches);
--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,13 @@ import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './ReportTarget/ConsoleReport';
 import { WinsAnalysis } from './Analyzers/WinsAnalyzer';
 import { Summary } from './Summary';
+import { HtmlReport } from './ReportTarget/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 
 const matchreader = new MatchReader(csvFileReader);
 matchreader.load();
 
-// let manUnitedWins = 0;
-
-// for (let match of matchreader.matches) {
-//   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-//     manUnitedWins++;
-//   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-//     manUnitedWins++;
-//   }
-// }
-
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 
 summary.buildAndPringReport(matchreader.matches);
